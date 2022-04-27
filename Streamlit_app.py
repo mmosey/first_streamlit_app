727d792f38fbeca46e0f45d7378d421d49084b1f
Mike Mosey <104029322+mmosey@users.noreply.github.com>
2022-04-27 14:01:36 -0400

    Update Streamlit_app.py
    add csv from S3 bucket

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -11,3 +11,5 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🍊 Build Your Own Fruit Smoothie 🍏🍇')
 
 import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
